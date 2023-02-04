972c9fb00d78879da8e97f69d1c3702a575e86bc
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2023-02-04 16:32:24 -0500

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/MelodyManager.cs
+++ Assets/Scripts/MelodyManager.cs
@@ -2,106 +2,134 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MelodyManager : MonoBehaviour {
+public class MelodyManager : MonoBehaviour
+{
 
-    public int[] melody;
-    public NoteSpot[] notesLineRef;
+	public int[] melody;
+	public NoteSpot[] notesLineRef;
 
-    int currentNote;
-    Cursor cursor;
-    bool hasFailed;
-    List<List<NoteSpot>> notesPerColumn;
+	int currentNote;
+	Cursor cursor;
+	bool hasFailed;
+	List<List<NoteSpot>> notesPerColumn;
 
 
-    public void Init() {
-        print("Start of" + gameObject.name);
-        if(melody.Length != notesLineRef.Length) {
-            throw new System.Exception("Melody " + gameObject.name +
-                " has different numbers of notes in its melody (" + melody.Length.ToString() +
-                ") and its reference line (" + notesLineRef.Length + ")");
-        }
-        cursor = FindObjectOfType<Cursor>();
+	public void Init()
+	{
+		print("Start of" + gameObject.name);
+		if (melody.Length != notesLineRef.Length)
+		{
+			throw new System.Exception("Melody " + gameObject.name +
+				" has different numbers of notes in its melody (" + melody.Length.ToString() +
+				") and its reference line (" + notesLineRef.Length + ")");
+		}
+		cursor = FindObjectOfType<Cursor>();
 
-        // Find what notes are in each column
-        notesPerColumn = new List<List<NoteSpot>>();
-        for (int i = 0; i < notesLineRef.Length; ++i) {
-            notesLineRef[i].Init();
+		// Find what notes are in each column
+		notesPerColumn = new List<List<NoteSpot>>();
+		for (int i = 0; i < notesLineRef.Length; ++i)
+		{
+			notesLineRef[i].Init();
 
-            List<NoteSpot> columnNotes = new List<NoteSpot>();
-            RaycastHit2D[] hits = Physics2D.RaycastAll(notesLineRef[i].transform.position, Vector2.down);
+			List<NoteSpot> columnNotes = new List<NoteSpot>();
+			RaycastHit2D[] hits = Physics2D.RaycastAll(notesLineRef[i].transform.position, Vector2.down);
 
-            foreach (RaycastHit2D hit in hits) {
-                NoteSpot noteSpot = hit.collider.gameObject.GetComponent<NoteSpot>();
-                if (noteSpot != null && noteSpot != notesLineRef[i]) {
-                    noteSpot.Init();
-                    columnNotes.Add(noteSpot);
-                }
-            }
+			foreach (RaycastHit2D hit in hits)
+			{
+				NoteSpot noteSpot = hit.collider.gameObject.GetComponent<NoteSpot>();
+				if (noteSpot != null && noteSpot != notesLineRef[i])
+				{
+					noteSpot.Init();
+					columnNotes.Add(noteSpot);
+				}
+			}
 
-            notesPerColumn.Add(columnNotes);
-        }
+			notesPerColumn.Add(columnNotes);
+		}
 
-        // Tell each note what column they are in
-        for (int i = 0; i < notesPerColumn.Count; ++i) {
-            foreach (NoteSpot noteSpot in notesPerColumn[i]) {
-                noteSpot.column = i;
-            }
-        }
+		// Tell each note what column they are in
+		for (int i = 0; i < notesPerColumn.Count; ++i)
+		{
+			foreach (NoteSpot noteSpot in notesPerColumn[i])
+			{
+				noteSpot.column = i;
+			}
+		}
 
-        // Set right note for the reference track AND one random note in its column
-        for (int i = 0; i < melody.Length; ++i) {
-            int randomIndex = Random.Range(0, notesPerColumn[i].Count);
-            notesPerColumn[i][randomIndex].SetNoteIndex(melody[i]);
-            notesLineRef[i].SetNoteIndex(melody[i]);
-            notesLineRef[i].reference = true;
-        }
-    }
+		// Set right note for the reference track AND one random note in its column
+		for (int i = 0; i < melody.Length; ++i)
+		{
+			int randomIndex = Random.Range(0, notesPerColumn[i].Count);
+			notesPerColumn[i][randomIndex].SetNoteIndex(melody[i]);
+			notesLineRef[i].SetNoteIndex(melody[i]);
 
-    public void ResetCursor() {
-        cursor.Reset();
-        hasFailed = false;
-        currentNote = 0;
-        for (int i = 0; i < notesPerColumn.Count; ++i) {
-            foreach (NoteSpot noteSpot in notesPerColumn[i]) {
-                noteSpot.Show();
-            }
-        }
-    }
+			notesPerColumn[i][randomIndex].isCurrentColorNote = true;
+			notesLineRef[i].isCurrentColorNote = true;
 
-    public void StartPlay() {
-        cursor.Play();
-    }
+			notesLineRef[i].isReferenceNote = true;
+		}
+	}
 
-    public void RegisterNote(NoteSpot noteSpot) {
-        int noteIndex = noteSpot.GetNoteIndex();
+	public void ResetCursor()
+	{
+		cursor.Reset();
+		hasFailed = false;
+		currentNote = 0;
+		for (int i = 0; i < notesPerColumn.Count; ++i)
+		{
+			foreach (NoteSpot noteSpot in notesPerColumn[i])
+			{
+				noteSpot.Show();
+			}
+		}
+	}
 
-        if (melody[currentNote] == noteIndex) {
-            //print("Note is valid (" + (currentNote + 1) + "/" + melody.Length + ")");
-        } else {
-            //print("Wrong note !  (" + (currentNote + 1) + "/" + melody.Length + ")");
-            hasFailed = true;
-        }
+	public void StartPlay()
+	{
+		cursor.Play();
+	}
 
-        currentNote++;
+	public void RegisterNote(NoteSpot noteSpot)
+	{
+		int noteIndex = noteSpot.GetNoteIndex();
 
-        if (currentNote == melody.Length) {
-            if (hasFailed) {
-                print("Melody failed");
-            } else {
-                EndMelody();
-            }
-        }
+		if (melody[currentNote] == noteIndex)
+		{
+			//print("Note is valid (" + (currentNote + 1) + "/" + melody.Length + ")");
+		}
+		else
+		{
+			//print("Wrong note !  (" + (currentNote + 1) + "/" + melody.Length + ")");
+			hasFailed = true;
+		}
 
-        foreach (NoteSpot noteSpot1 in notesPerColumn[noteSpot.column]) {
-            if (noteSpot1 != noteSpot) {
-                noteSpot1.Hide();
-            }
-        }
-    }
+		currentNote++;
 
-    void EndMelody() {
-        print(gameObject.name + " succeeded !");
-        GetComponentInParent<LevelManager>().GoToNextMelody();
-    }
+		if (currentNote == melody.Length)
+		{
+			if (hasFailed)
+			{
+				print("Melody failed");
+			}
+			else
+			{
+				EndMelody();
+			}
+		}
+
+		foreach (NoteSpot noteSpot1 in notesPerColumn[noteSpot.column])
+		{
+			if (noteSpot1 != noteSpot)
+			{
+				noteSpot1.Hide();
+			}
+		}
+	}
+
+	void EndMelody()
+	{
+		print(gameObject.name + " succeeded !");
+		GetComponentInParent<LevelManager>().GoToNextMelody();
+	}
 
 }
--- Assets/Scripts/NoteSpot.cs
+++ Assets/Scripts/NoteSpot.cs
@@ -2,88 +2,127 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoteSpot : MonoBehaviour {
-    [SerializeField] private AudioClip[] notes;
-    [SerializeField] private Color[] noteColors;
+public class NoteSpot : MonoBehaviour
+{
+	[SerializeField] private AudioClip[] notes;
+	[SerializeField] private Color[] noteColors;
 
-    [SerializeField] private bool isMovingVertical;
-    [SerializeField] private bool isMovingHorizontal;
-    [SerializeField] private float speedMov = 2f;
-    [SerializeField] private float heightMov = 0.005f;
+	// Lignes/colonnes bougent
+	[SerializeField] private bool isMovingVertical;
+	[SerializeField] private bool isMovingHorizontal;
+	[SerializeField] private float speedMov = 2f;
+	[SerializeField] private float heightMov = 0.005f;
+	// Les notes alternent entre 2 couleurs
+	[SerializeField] private bool isAlternateColor;
+	[SerializeField] private float alternateColorDelay = 1000f;
+	private int alternateColorNoteIndex;
+	[HideInInspector] public bool isCurrentColorNote;
 
-    private AudioSource source;
-    private SpriteRenderer sprite;
-    private MelodyManager melodyManager;
-    int noteIndex;
-    bool isHidden;
-    Vector3 initalPos;
-    [HideInInspector] public int column;
-    [HideInInspector] public bool reference;
+	private AudioSource source;
+	private SpriteRenderer sprite;
+	private MelodyManager melodyManager;
+	private int noteIndex;
+	bool isHidden;
+	Vector3 initalPos;
+	[HideInInspector] public int column;
+	[HideInInspector] public bool isReferenceNote;
 
-    private float dec = 0.0001f;
+	private float dec = 0.0001f;
 
-    public void Init() {
-        source = GetComponent<AudioSource>();
-        sprite = GetComponentInChildren<SpriteRenderer>();
-        melodyManager = FindObjectOfType<MelodyManager>();
+	public void Init()
+	{
+		source = GetComponent<AudioSource>();
+		sprite = GetComponentInChildren<SpriteRenderer>();
+		melodyManager = FindObjectOfType<MelodyManager>();
 
-        SetNoteIndex(Random.Range(0, notes.Length));
-        initalPos = transform.position;
-    }
+		SetNoteIndex(Random.Range(0, notes.Length));
+		initalPos = transform.position;
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (!isHidden && collision.CompareTag("Cursor")) {
-            Play();
-            melodyManager.RegisterNote(this);
-        }
-    }
+		InvokeRepeating("AlternateColor", alternateColorDelay, alternateColorDelay);
+	}
 
-    void Play() {
-        source.clip = notes[noteIndex];
-        source.Play();
-    }
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (!isHidden && collision.CompareTag("Cursor"))
+		{
+			Play();
+			melodyManager.RegisterNote(this);
+		}
+	}
 
-    public void Hide() {
-        Color hiddenColor = sprite.color;
-        hiddenColor.a = 0.5f;
-        sprite.color = hiddenColor;
-        isHidden = true;
-    }
+	void Play()
+	{
+		source.clip = notes[noteIndex];
+		source.Play();
+	}
 
-    public void Show() {
-        Color normalColor = sprite.color;
-        normalColor.a = 1f;
-        sprite.color = normalColor;
-        isHidden = false;
-    }
+	public void Hide()
+	{
+		Color hiddenColor = sprite.color;
+		hiddenColor.a = 0.5f;
+		sprite.color = hiddenColor;
+		isHidden = true;
+	}
 
-    public void SetNoteIndex(int value) {
-        noteIndex = value;
-        sprite.color = noteColors[noteIndex];
-    }
+	public void Show()
+	{
+		Color normalColor = sprite.color;
+		normalColor.a = 1f;
+		sprite.color = normalColor;
+		isHidden = false;
+	}
 
-    public int GetNoteIndex() {
-        return noteIndex;
-    }
+	public void SetNoteIndex(int value)
+	{
+		noteIndex = value;
+		sprite.color = noteColors[noteIndex];
+	}
 
-    private void Update() {
-        if (!reference) {
-            Vector3 pos = transform.position;
-            float newY = initalPos.y;
-            float newX = initalPos.x;
+	public void SetSpriteColor(int noteIndex)
+	{
+		sprite.color = noteColors[noteIndex];
+	}
 
-            if (isMovingVertical) {
-                newY = (Mathf.Sin(Time.time * speedMov) * heightMov) + pos.y;
-            }
-            if (isMovingHorizontal) {
-                newX = (Mathf.Sin(Time.time * speedMov) * heightMov) + pos.x;
-            }
+	public int GetNoteIndex()
+	{
+		return noteIndex;
+	}
 
-            transform.position = new Vector3(newX, newY, pos.z);
-        }
-    }
+	private void Update()
+	{
+		if (!isReferenceNote)
+		{
+			Vector3 pos = transform.position;
+			float newY = initalPos.y;
+			float newX = initalPos.x;
 
-    private Vector3 RandomizePosition(float minX, float maxX, float minY, float maxY, float Z) {
-        return new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Z);
-    }
+			if (isMovingVertical)
+			{
+				newY = (Mathf.Sin(Time.time * speedMov) * heightMov) + pos.y;
+			}
+			if (isMovingHorizontal)
+			{
+				newX = (Mathf.Sin(Time.time * speedMov) * heightMov) + pos.x;
+			}
+			transform.position = new Vector3(newX, newY, pos.z);
+		}
+	}
+
+	private void AlternateColor()
+	{
+		if (!isReferenceNote)
+		{
+			if (isCurrentColorNote)
+			{
+				// alternateColorNoteIndex = Random.Range(0, noteColors.Length);
+				SetSpriteColor(2);
+				isCurrentColorNote = !isCurrentColorNote;
+			}
+			else
+			{
+				SetSpriteColor(noteIndex);
+				isCurrentColorNote = !isCurrentColorNote;
+			}
+		}
+	}
 }
