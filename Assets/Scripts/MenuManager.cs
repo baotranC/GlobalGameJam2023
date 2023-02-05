24a542d13fc60d41af0ac92fc332955105791d5e
Yumahey <yumalepro@gmail.com>
2023-02-05 15:19:18 -0500

    Main menu scene transition

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,15 +1,32 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
-{
-	public void Quit()
-	{
-		Application.Quit();
-	}
+public class MenuManager : MonoBehaviour {
 
-	public void Play()
-	{
-		SceneManager.LoadScene("Main");
-	}
+    public Animator sceneTransition;
+    public GameObject rideauGO;
+
+    public void Quit() {
+        StartCoroutine(PerformQuit());
+    }
+
+    IEnumerator PerformQuit() {
+        yield return new WaitForSeconds(0.5f);
+        Application.Quit();
+    }
+
+    public void Play() {
+        StartCoroutine(PerformPlay());
+    }
+
+    IEnumerator PerformPlay() {
+        rideauGO.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        sceneTransition.SetTrigger("FadeIn");
+
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene("Main");
+    }
 }
