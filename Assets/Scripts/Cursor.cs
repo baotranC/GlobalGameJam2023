adfe8787a62fda632d125899134c153dacdea484
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2023-02-05 11:08:16 -0500

    Add reset melody

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -3,45 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Cursor : MonoBehaviour {
+public class Cursor : MonoBehaviour
+{
 
-    public GameObject[] darkness;
+	public GameObject[] darkness;
 
-    Rigidbody2D rb;
-    float moveInput;
-    Vector3 initialPos;
-    [HideInInspector] public float horizontalSpeed = 2f;
-    [HideInInspector] public float verticalSpeed = 4f;
+	Rigidbody2D rb;
+	float moveInput;
+	Vector3 initialPos;
+	[HideInInspector] public float horizontalSpeed = 2f;
+	[HideInInspector] public float verticalSpeed = 4f;
 
 
-    void Awake() {
-        rb = GetComponent<Rigidbody2D>();
-        initialPos = transform.position;
-    }
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		initialPos = transform.position;
+	}
 
-    public void Reset() {
-        transform.position = initialPos;
-        rb.velocity = Vector2.zero;
-    }
+	public void Reset()
+	{
+		transform.position = initialPos;
+		rb.velocity = Vector2.zero;
+	}
 
-    public void Play() {
-        rb.velocity = Vector2.right * horizontalSpeed;
-    }
+	public void Play()
+	{
+		rb.velocity = Vector2.right * horizontalSpeed;
+	}
 
-    public void PlayInput(InputAction.CallbackContext context) {
-        if(context.started) {
-            Play();
-        }
-    }
+	public void PlayInput(InputAction.CallbackContext context)
+	{
+		if (context.started)
+		{
+			Play();
+		}
+	}
 
-    public void MoveInput(InputAction.CallbackContext context) {
-        moveInput = context.ReadValue<float>();
-        rb.velocity = new Vector2(rb.velocity.x, moveInput * verticalSpeed);
-    }
+	public void MoveInput(InputAction.CallbackContext context)
+	{
+		moveInput = context.ReadValue<float>();
+		rb.velocity = new Vector2(rb.velocity.x, moveInput * verticalSpeed);
+	}
 
-    public void SetDarkness(bool value) {
-        foreach(GameObject go in darkness) {
-            go.SetActive(value);
-        }
-    }
+	public void SetDarkness(bool value)
+	{
+		foreach (GameObject go in darkness)
+		{
+			go.SetActive(value);
+		}
+	}
 }
